d5a61504d92b0be561017059ccc85e2ecc3f604d
Adrian <113379785+Havorr@users.noreply.github.com>
2026-01-11 14:47:53 +0100

    Update main.cpp

--- A-hello-cmake/main.cpp
+++ A-hello-cmake/main.cpp
@@ -6,8 +6,9 @@ int main(int argc, char *argv[])
     std::cout << "Hello CMake!" << std::endl;
     std::cout << "2 + 3 = " << add(2, 3) << std::endl;
 
+    int* p = new int[1];
+    delete[] p;
+    p[0] = 1; // USE_AFTER_FREE (Coverity to wykryje)
+
     return 0;
 }
-
-int* p = nullptr;
-*p = 1;
